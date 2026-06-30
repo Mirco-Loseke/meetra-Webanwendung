465074bc51141371694ca4476e537433631d4188
Mirco-Loseke <loseke.mirco@gmail.com>
2026-06-30 09:28:53 +0200

    Delete protocol_templates_category_migration.sql

--- protocol_templates_category_migration.sql
+++
@@ -1,6 +0,0 @@
--- Add category_id to protocol_templates table
-ALTER TABLE protocol_templates ADD COLUMN IF NOT EXISTS category_id bigint REFERENCES categories(id) ON DELETE SET NULL;
-
--- Update existing templates if possible (placeholders until we know exact IDs)
--- For now, let's just add the column. 
--- The user can associate them via the UI we're about to build.
