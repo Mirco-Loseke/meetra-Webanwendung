8475bd550ac6c50f5c8e844f48824cb14a9ed94d
Mirco-Loseke <loseke.mirco@gmail.com>
2026-06-30 09:28:35 +0200

    Delete protocol_templates_type_migration.sql

--- protocol_templates_type_migration.sql
+++
@@ -1,7 +0,0 @@
--- Add type column to protocol_templates table
-ALTER TABLE protocol_templates ADD COLUMN IF NOT EXISTS type text;
-
--- Add check constraint to ensure only allowed types (optional but recommended)
--- ALTER TABLE protocol_templates ADD CONSTRAINT check_template_type CHECK (type IN ('intake', 'acceptance'));
-
-COMMENT ON COLUMN protocol_templates.type IS 'Distinguishes between intake (Eingang) and acceptance (Abnahme) templates';
