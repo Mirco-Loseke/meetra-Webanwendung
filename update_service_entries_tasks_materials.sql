183479d428609ea73045bfbf88a008b7f68620e3
Mirco-Loseke <loseke.mirco@gmail.com>
2026-06-30 09:22:43 +0200

    Delete update_service_entries_tasks_materials.sql

--- update_service_entries_tasks_materials.sql
+++
@@ -1,3 +0,0 @@
--- Add tasks and materials JSONB tables to service_entries table
-ALTER TABLE service_entries ADD COLUMN IF NOT EXISTS tasks jsonb;
-ALTER TABLE service_entries ADD COLUMN IF NOT EXISTS materials jsonb;
