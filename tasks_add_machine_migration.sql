05b37ab088251624a8a19a13de8b3d472ec364e2
Mirco-Loseke <loseke.mirco@gmail.com>
2026-06-30 09:26:50 +0200

    Delete tasks_add_machine_migration.sql

--- tasks_add_machine_migration.sql
+++
@@ -1,5 +0,0 @@
--- Migration: Add machine_id to tasks table
--- This replaces the project_id concept to tie tasks directly to machines
-
-ALTER TABLE public.tasks 
-ADD COLUMN IF NOT EXISTS machine_id BIGINT REFERENCES public.machines(id) ON DELETE SET NULL;
