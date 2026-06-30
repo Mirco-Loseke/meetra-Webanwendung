195c58a588ab072ef33b047ba30dff17c8925d2f
Mirco-Loseke <loseke.mirco@gmail.com>
2026-06-30 09:25:50 +0200

    Delete procurements_add_files_migration.sql

--- procurements_add_files_migration.sql
+++
@@ -1,6 +0,0 @@
--- Migration: Add JSONB files column to procurements
--- This column will store an array of file objects, similar to the machines table.
--- Format: [{ url: '...', name: '...', type: '...' }]
-
-ALTER TABLE public.procurements 
-ADD COLUMN IF NOT EXISTS files JSONB DEFAULT '[]'::jsonb;
