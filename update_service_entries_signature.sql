718017994ea98850f154cc0590f18c748557f874
Mirco-Loseke <loseke.mirco@gmail.com>
2026-06-30 09:23:20 +0200

    Delete update_service_entries_signature.sql

--- update_service_entries_signature.sql
+++
@@ -1,5 +0,0 @@
--- Add travel distance, travel time, signature, and signee name to service_entries table
-ALTER TABLE service_entries ADD COLUMN IF NOT EXISTS travel_distance_km numeric(6,2);
-ALTER TABLE service_entries ADD COLUMN IF NOT EXISTS travel_time_minutes integer;
-ALTER TABLE service_entries ADD COLUMN IF NOT EXISTS customer_signature text;
-ALTER TABLE service_entries ADD COLUMN IF NOT EXISTS customer_name text;
