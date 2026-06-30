af61a390e24c4ee0ad7e55a1306530587f140d69
Mirco-Loseke <loseke.mirco@gmail.com>
2026-06-30 09:29:47 +0200

    Delete add_remark_to_internal_processes.sql

--- add_remark_to_internal_processes.sql
+++
@@ -1,3 +0,0 @@
--- Separates Bemerkungs-/Beschreibungsfeld fuer interne Vorgaenge (zusaetzlich zum E-Mail-Text in 'description')
-ALTER TABLE internal_processes
-ADD COLUMN IF NOT EXISTS remark text;
